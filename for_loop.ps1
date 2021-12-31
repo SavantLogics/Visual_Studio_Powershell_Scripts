a1d6e3be44f02081ae0d8da6f5b930947442e610
SavantLogics <rsaviant@gmail.com>
2021-12-30 23:09:20 -0800

    added text for output object

--- for_loop.ps1
+++ for_loop.ps1
@@ -1,6 +1,8 @@
-$output = "Some random text or file"
+## Use a FOR loop and output the results to a file 10 times
+
+$output = "You Are Little More Than A Immediately Inbred Toad And A Quarrelsome Ill-Nurtured Scut. Never Have I Encountererd Such An Axiomatic Hedge-Born Canker-Blossom. My Good Man, You Are But A Currish Rough-hewn Flirt-gill. You Really Are A Magnificent Plume-plucked Dingus. Never Have I Encountererd Such A Even Clay-brained Affliction. My Dear Lady, You Are But A Bootless Badly Evolved Giglet. Never Have I Encountererd Such A Valuable Dread-Bolted Wet Lettuce. My Good Man, You Are But A Tearful Onion-Eyed Dingleberry. Madam, You Are Little More Than A Abrasive Fen-sucked Chimpanzee."
 
 For($index = 1; $index -le 10; $index++ )
 {
-$output >> "C:\Users\rhawkins\Documents\text_files\creating_Large_Text_Files.txt"
+$output >> "D:\Documents\Testing_Files\random_generated_files.txt"
 }

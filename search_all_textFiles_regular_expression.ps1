d1917903bcd18b0e6cec4e1977850279715e01c5
SavantLogics <37237071+SavantLogics@users.noreply.github.com>
2023-05-18 22:07:40 -0500

    Update search_all_textFiles_regular_expression.ps1

--- search_all_textFiles_regular_expression.ps1
+++ search_all_textFiles_regular_expression.ps1
@@ -1,1 +1,22 @@
+Get-ChildItem *.txt -Recurse: This gets all .txt files in the current directory and all subdirectories.
+Select-String SearchPattern: This searches within the files for lines that match the pattern "SearchPattern".
+
 Get-ChildItem *.txt -Recurse | Select-String SearchPattern
+
+Search Case Insensitive: By default, Select-String is case-sensitive. If you want to perform a case-insensitive search, you can use the -CaseSensitive switch:
+Get-ChildItem *.txt -Recurse | Select-String -Pattern SearchPattern -CaseSensitive
+
+Include Line Numbers: If you want to include line numbers in the output, use the -Context parameter:
+Get-ChildItem *.txt -Recurse | Select-String -Pattern SearchPattern -Context 0
+
+Quiet Mode: If you only want to know whether or not a match exists, you can use the -Quiet switch:
+Get-ChildItem *.txt -Recurse | Select-String -Pattern SearchPattern -Quiet
+
+Specify Search Pattern as a Regular Expression: You can use regular expressions as your search pattern:
+Get-ChildItem *.txt -Recurse | Select-String -Pattern '^\d+'
+
+This would search for lines beginning with one or more digits.
+
+Remember to replace SearchPattern with the actual text or pattern you want to find.
+
+Please note, these commands can be computationally intensive if you have a large number of files or if the files are very large. Use them judiciously.
